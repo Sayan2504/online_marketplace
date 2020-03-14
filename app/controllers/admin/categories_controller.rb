780276af1d84aefe48916097c35d7e956ca43378
Sayan Sinha <sinhasayan888@gmail.com>
2020-03-14 14:40:08 +0530

    category can be added now using form

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,33 @@
 class Admin::CategoriesController < ApplicationController
+    before_action :set_format, only: [:show]
+
+    def index
+        @categories = Category.all
+    end 
+
     def new
         @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+
+        if @category.save
+            flash[:success] = "Category has been successfully created"
+            redirect_to admin_category_path(@category)
+        else
+            render "new"
+        end
+    end
+
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
+    def set_format
+        @category = Category.find(params[:id])
     end
 end
