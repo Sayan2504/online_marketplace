ba58f00fcefb8d4ad0f0318ce71e77c1b40a1ba0
Sayan Sinha <sinhasayan888@gmail.com>
2020-03-15 18:10:21 +0530

    Images can be resized

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 
-    #has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-    has_attached_file :image
+    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    #has_attached_file :image
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
     belongs_to :user, optional: true
