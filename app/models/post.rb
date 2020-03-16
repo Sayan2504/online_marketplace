dd6018003348f0f53a4e231d56b1ad29f8de641f
Sayan Sinha <sinhasayan888@gmail.com>
2020-03-16 17:56:58 +0530

    adding post from any user sending it to admin for approval

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
 
     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-    #has_attached_file :image
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
     belongs_to :user, optional: true
