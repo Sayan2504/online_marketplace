dd9429f2385fb291750a09572f0c9bac2a1f90bc
Sayan Sinha <sinhasayan888@gmail.com>
2020-03-22 02:25:06 +0530

    all post regexp validations done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,38 @@
 class Post < ApplicationRecord
-    VALID_AD_TITLE_REGEX = /\A[\w\d]+[\s]?{1}[\w\d]+\z/i.freeze
+    VALID_TITLE_REGEX = /\A[\w\d]+[\s\d\w+]*\z/i.freeze
+    VALID_DESCRIPTION_REGEX = /\A[\w\d]+[-()]?[\s]?[\d\w+]*\z/i.freeze
+    VALID_USERNAME_REGEX = /\A[\w\d]+[\w\d\s_]*\z/i.freeze
+    VALID_PHONE_REGEX = /\A[1-9]{0,3}-[6-9]{1}[0-9]{9}\z/i.freeze
+    VALID_CITY_REGEX = /\A[\w]+[\s\w]*\z/i.freeze
 
+    
     belongs_to :user, optional: true
     belongs_to :category
 
     has_many :post_attachments
     accepts_nested_attributes_for :post_attachments
 
-    validates :ad_title, presence: true, length: {maximum: 16, minimum: 2}, format: {with: VALID_AD_TITLE_REGEX}
-    validates :detailed_ad_title, presence: true, length: {maximum: 30, minimum: 2}
-    validates :ad_description, presence: true, length: {maximum: 255, minimum: 2}
-    validates :user_name, presence: true, length: {maximum: 30, minimum: 2}
-    validates :phone, presence: true, length: {maximum: 15, minimum: 10}
-    validates :city, presence: true, length: {maximum: 50, minimum: 1}
-    
-    
+    validates :ad_title, presence: true, 
+                         length: {maximum: 16, minimum: 2}, 
+                         format: {with: VALID_TITLE_REGEX}
+
+    validates :detailed_ad_title, presence: true, 
+                                  length: {maximum: 30, minimum: 2},
+                                  format: {with: VALID_TITLE_REGEX}
+
+    validates :ad_description, presence: true, 
+                               length: {maximum: 255, minimum: 2},
+                               format: {with: VALID_DESCRIPTION_REGEX}
+
+    validates :user_name, presence: true, 
+                          length: {maximum: 30, minimum: 2},
+                          format: {with: VALID_USERNAME_REGEX}
+
+    validates :phone, presence: true, 
+                      length: {maximum: 15, minimum: 10},
+                      format: {with: VALID_PHONE_REGEX}
+
+    validates :city, presence: true, 
+                     length: {maximum: 50, minimum: 1},
+                     format: {with: VALID_CITY_REGEX}   
 end
