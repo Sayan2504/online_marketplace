2c6097c69406b8bfb8fa62c04e68ff7cce1eb5a2
Sayan Sinha <sinhasayan888@gmail.com>
2020-03-24 02:11:13 +0530

    current user cannot his own post,others can review approved post, admin doesnt have review button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
     end     
 
     def show
+        @user = current_user
         @post_attachments = @post.post_attachments.all
         @reviews = @post.reviews.all
     end
