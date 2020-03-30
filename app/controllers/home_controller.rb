7c13ed75615cfcb40d88b77b21bed53e356460f2
Sayan Sinha <sinhasayan888@gmail.com>
2020-03-31 03:33:48 +0530

    choosing posts by category on homepage(half done)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,11 @@ class HomeController < ApplicationController
 
 
   def index
-   @posts = Post.select(["approved_by", "id", "ad_title"]).where.not(approved_by: ['null', 'rejected'])
+    if params[:category]
+      @posts = Post.select(["approved_by", "id", "ad_title"]).where.not(approved_by: ['null', 'rejected']).where(:category => params[:category])
+    else
+      @posts = Post.select(["approved_by", "id", "ad_title"]).where.not(approved_by: ['null', 'rejected'])
+    end
   end
 
   private 
