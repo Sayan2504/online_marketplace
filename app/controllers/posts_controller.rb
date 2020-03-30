9aebabb90815ee8e11b89011757fecf813aeb2af
Sayan Sinha <sinhasayan888@gmail.com>
2020-03-31 03:18:55 +0530

    if pic missing then missing else the pic uploded(sorted)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,11 +22,9 @@ class PostsController < ApplicationController
         
 
         if @post.save
-          if @post.post_attachments.present?
             params[:post_attachments]['photo'].each do |a|
-              @post_attachment = @post.post_attachments.create(:photo => a, :post_id => @post.id, :user_id => current_user.id)
+                @post_attachment = @post.post_attachments.create(:photo => a, :post_id => @post.id, :user_id => current_user.id)
             end
-          end
             flash[:success] = "Post has been successfully created"
             redirect_to post_path(@post)
         else
