5654a28e805ba0ff7f042b3d07723b3f60efbee2
Sayan Sinha <sinhasayan888@gmail.com>
2020-03-31 12:42:41 +0530

    categries dropdown in homepage implemented

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,8 @@ class HomeController < ApplicationController
 
 
   def index
-    if params[:category]
-      @posts = Post.select(["approved_by", "id", "ad_title"]).where.not(approved_by: ['null', 'rejected']).where(:category => params[:category])
-    else
-      @posts = Post.select(["approved_by", "id", "ad_title"]).where.not(approved_by: ['null', 'rejected'])
-    end
+    @posts = Post.select(["approved_by", "id", "ad_title"]).where.not(approved_by: ['null', 'rejected'])
+    @categories = Category.all
   end
 
   private 
