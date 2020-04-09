eb8bbc27724b7536bb12de7bbb0219cab0d7f659
Sayan Sinha <sinhasayan888@gmail.com>
2020-04-10 03:01:06 +0530

    one user can make a single buying request for a single post

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -8,16 +8,24 @@ class BuyersController < ApplicationController
 
     def create
         @buyer = Buyer.new(buyer_params)
-        @buyer_id = User.select(["email"]).find_by(email: @buyer.email)
-        if @buyer_id.present?
-            if @buyer.save
-                flash[:success] = "Your buying request has been send to the author of the post"
+        @user = User.select(["email", "name"]).find_by(email: @buyer.email)
+        @post = Post.select(["id", "ad_title"]).find_by(id: @buyer.post_id)
+        @buyer_user = Buyer.select(["email"]).find_by(email: @buyer.email)
+
+        if @user.present?
+            if @buyer_user.present?
+                flash[:warning] = "You already have a buying request for this post"
                 redirect_to post_path(@buyer.post_id)
             else
-                render "new"
+                if @buyer.save
+                    flash[:success] = "Your buying request has been send to the author of the post #{@user.name}"
+                    redirect_to post_path(@buyer.post_id)
+                else
+                    render "new"
+                end
             end
         else
-            flash[:warning] = "Please register yourself before buying"
+            flash[:warning] = "Please register yourself before buying #{@post.ad_title}"
             redirect_to signup_path
         end
     end
