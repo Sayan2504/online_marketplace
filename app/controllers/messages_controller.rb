37b81abe05305b256d9c13809002e95e593b72fb
Sayan Sinha <sinhasayan888@gmail.com>
2020-04-16 13:15:45 +0530

    indian timezone updated in chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,26 @@
 class MessagesController < ApplicationController
   
   def new
-    @messages = Message.where(user_id: params[:sender_id])
-    @messages = @messages.where(post_id: params[:post_id])
+    #showing the chat thread
+    @messages = Message.where(user_id: params[:sender_id]) #filtering based on the sender for a particular message in chat
+    @messages = @messages.where(post_id: params[:post_id]) #filtering based on the post for a particular message in chat
 
-    @post = Post.find(params[:post_id])
+    #getting the params to be used during create
+    @post = Post.find(params[:post_id]) #finding the post based on url params
 
-    @message = Message.new
+    @message = Message.new  #rendering a new message
   end
 
   def create
     @message = Message.new(message_params)
-    @message.user = current_user
+    @message.user = current_user #populating the user_id field in messages table
 
+    #populating the post_id field in messages table
     @posts_current_user = current_user.posts
     @posts_current_user = @posts_current_user.find_by(id: @message.post_id)
     @message.post = @posts_current_user
 
-    #@message.post = current_user.posts.find_by!(id: params[:post_id])
+    #saving based on params received from form
     @message.save
     
   end
