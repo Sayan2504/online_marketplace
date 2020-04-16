728d99bf04d0e67202d7291dcf0666a9ea14fd6c
Sayan Sinha <sinhasayan888@gmail.com>
2020-04-16 12:38:09 +0530

    resolved the problem of separate js file for ratings

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("packs/ratings")
 //= require jquery3
 //= require popper
 //= require bootstrap
