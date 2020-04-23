cbf27d0a1690aeb2dfa2d4b52d8e29b1dce860b8
Sayan Sinha <sinhasayan888@gmail.com>
2020-04-23 21:03:51 +0530

    rating star view made DRY

--- app/javascript/packs/ratings.js
+++ app/javascript/packs/ratings.js
@@ -1,56 +1,57 @@
 
-      window.addEventListener('click', function(event) {
-        if (event.target.classList.contains('check1')){
-          document.getElementById('star_1').classList.remove('activated');
-          document.getElementById('star_2').classList.remove('activated');
-          document.getElementById('star_3').classList.remove('activated');
-          document.getElementById('star_4').classList.remove('activated');
-          document.getElementById('star_5').classList.remove('activated');
+window.addEventListener('click', function(event) {
+  
+  if (event.target.classList.contains('check1')){
+    document.getElementById('star_1').classList.remove('activated');
+    document.getElementById('star_2').classList.remove('activated');
+    document.getElementById('star_3').classList.remove('activated');
+    document.getElementById('star_4').classList.remove('activated');
+    document.getElementById('star_5').classList.remove('activated');
 
-          document.getElementById('star_1').classList.add('activated');
-        }
+    document.getElementById('star_1').classList.add('activated');
+  }
 
-        if (event.target.classList.contains('check2')){
-          document.getElementById('star_1').classList.remove('activated');
-          document.getElementById('star_2').classList.remove('activated');
-          document.getElementById('star_3').classList.remove('activated');
-          document.getElementById('star_4').classList.remove('activated');
-          document.getElementById('star_5').classList.remove('activated');
+  if (event.target.classList.contains('check2')){
+    document.getElementById('star_1').classList.remove('activated');
+    document.getElementById('star_2').classList.remove('activated');
+    document.getElementById('star_3').classList.remove('activated');
+    document.getElementById('star_4').classList.remove('activated');
+    document.getElementById('star_5').classList.remove('activated');
 
-          document.getElementById('star_1').classList.add('activated');
-          document.getElementById('star_2').classList.add('activated');
-        }
+    document.getElementById('star_1').classList.add('activated');
+    document.getElementById('star_2').classList.add('activated');
+  }
 
-        if (event.target.classList.contains('check3')){
-          document.getElementById('star_1').classList.remove('activated');
-          document.getElementById('star_2').classList.remove('activated');
-          document.getElementById('star_3').classList.remove('activated');
-          document.getElementById('star_4').classList.remove('activated');
-          document.getElementById('star_5').classList.remove('activated');
+  if (event.target.classList.contains('check3')){
+    document.getElementById('star_1').classList.remove('activated');
+    document.getElementById('star_2').classList.remove('activated');
+    document.getElementById('star_3').classList.remove('activated');
+    document.getElementById('star_4').classList.remove('activated');
+    document.getElementById('star_5').classList.remove('activated');
 
-          document.getElementById('star_1').classList.add('activated');
-          document.getElementById('star_2').classList.add('activated');
-          document.getElementById('star_3').classList.add('activated');
-        }
+    document.getElementById('star_1').classList.add('activated');
+    document.getElementById('star_2').classList.add('activated');
+    document.getElementById('star_3').classList.add('activated');
+  }
 
-        if (event.target.classList.contains('check4')){
-          document.getElementById('star_1').classList.remove('activated');
-          document.getElementById('star_2').classList.remove('activated');
-          document.getElementById('star_3').classList.remove('activated');
-          document.getElementById('star_4').classList.remove('activated');
-          document.getElementById('star_5').classList.remove('activated');
-          
-          document.getElementById('star_1').classList.add('activated');
-          document.getElementById('star_2').classList.add('activated');
-          document.getElementById('star_3').classList.add('activated');
-          document.getElementById('star_4').classList.add('activated');
-        }
+  if (event.target.classList.contains('check4')){
+    document.getElementById('star_1').classList.remove('activated');
+    document.getElementById('star_2').classList.remove('activated');
+    document.getElementById('star_3').classList.remove('activated');
+    document.getElementById('star_4').classList.remove('activated');
+    document.getElementById('star_5').classList.remove('activated');
+    
+    document.getElementById('star_1').classList.add('activated');
+    document.getElementById('star_2').classList.add('activated');
+    document.getElementById('star_3').classList.add('activated');
+    document.getElementById('star_4').classList.add('activated');
+  }
 
-        if (event.target.classList.contains('check5')){
-          document.getElementById('star_1').classList.add('activated');
-          document.getElementById('star_2').classList.add('activated');
-          document.getElementById('star_3').classList.add('activated');
-          document.getElementById('star_4').classList.add('activated');
-          document.getElementById('star_5').classList.add('activated');
-        }
-      });  
+  if (event.target.classList.contains('check5')){
+    document.getElementById('star_1').classList.add('activated');
+    document.getElementById('star_2').classList.add('activated');
+    document.getElementById('star_3').classList.add('activated');
+    document.getElementById('star_4').classList.add('activated');
+    document.getElementById('star_5').classList.add('activated');
+  }
+});  
