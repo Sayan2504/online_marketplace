0a61a01f2821c00ce3785029b583d887e0fc69b5
Sayan Sinha <sinhasayan888@gmail.com>
2020-05-02 02:40:53 +0530

    solved feedback4...Category names that the admin can create should be unique. Give option to edit category name as well as delete category if no post is present under it

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :update, :destroy]
 
   def create
     @category = Category.new(category_params)
@@ -10,6 +11,16 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  def destroy
+    if @category.delete
+      flash[:danger] = "Category has been successfully deleted"
+      redirect_to admin_categories_path
+    end  
+  end
+
+  def edit
+  end
+
   def index
     @categories = Category.all
   end 
@@ -18,9 +29,25 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def show
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = "Category has been successfully updated"
+      redirect_to admin_categories_path
+    else
+      render "edit"
+    end
+  end
+
   private
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
