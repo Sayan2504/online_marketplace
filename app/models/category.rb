4297f48a6f0e1eca8226323fd62bfdfdd214f0de
Sayan Sinha <sinhasayan888@gmail.com>
2020-05-01 21:48:29 +0530

    feedback4...categories unique

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,5 +4,5 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :posts
 
-  validates :name,  presence: true, length: { maximum: 14, minimum: 2 } , format: { with: VALID_CATEGORY_REGEX }
+  validates :name,  presence: true, length: { maximum: 14, minimum: 2 } , uniqueness: { case_sensitive: false }, format: { with: VALID_CATEGORY_REGEX }
 end
