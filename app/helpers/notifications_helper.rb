a109ecbff3fca3e52239743835cdf1346ca97b95
Sayan Sinha <sinhasayan888@gmail.com>
2020-05-03 18:33:46 +0530

    made unread messages post specific

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,5 +1,9 @@
 module NotificationsHelper
   def notification_view
-    Notification.where(receiver_id: current_user.id, unread: "true").reverse
+    if params[:post_id].present?
+      Notification.where(receiver_id: current_user.id, post_id: params[:post_id], unread: "true").reverse
+    else  
+      Notification.where(receiver_id: current_user.id, unread: "true").reverse
+    end  
   end
 end
