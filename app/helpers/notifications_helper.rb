8aa9b54e7ec86e16d9a1969e948fbe0759a57b9c
Sayan Sinha <sinhasayan888@gmail.com>
2020-05-03 20:06:03 +0530

    complete

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,7 +1,7 @@
 module NotificationsHelper
   def notification_view
     if params[:post_id].present?
-      Notification.where(receiver_id: current_user.id, post_id: params[:post_id], unread: "true").reverse
+      Notification.where(receiver_id: current_user.id, post_id: params[:post_id], unread: "true").reverse  
     else  
       Notification.where(receiver_id: current_user.id, unread: "true").reverse
     end  
