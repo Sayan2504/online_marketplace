5c215a8efaf14fe0cafa10adf21bc29008b4b4f6
Sayan Sinha <sinhasayan888@gmail.com>
2020-06-11 14:25:26 +0530

    updated validation test cases for category model(using let)

--- spec/models/buyer_spec.rb
+++ spec/models/buyer_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Buyer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    describe ".buyer_name" do
+      it "buyer having a name should be valid" do
+        buyer = Buyer.new(buyer_name: "Someone", email: "someone@gmail.com", location: "somewhere", post_id: 1, user_id: 1).save
+        expect(buyer).to eq(true)
+      end
+      it "buyer having no name should be invalid" do
+        buyer = Buyer.new(buyer_name: "", email: "someone@gmail.com", location: "somewhere", post_id: 1, user_id: 1).save
+        expect(buyer).to eq(false)
+      end 
+      
+      it "buyer having name of length within 2 and 30 characters should be valid" do
+        buyer = Buyer.new(buyer_name: "Someone", email: "someone@gmail.com", location: "somewhere", post_id: 1, user_id: 1).save
+        expect(buyer).to eq(true)
+      end
+
+      it "buyer not having name of length within 2 and 14 characters should be invalid" do
+        buyer = Buyer.new(buyer_name: "S", email: "someone@gmail.com", location: "somewhere", post_id: 1, user_id: 1).save
+        expect(buyer).to eq(false)
+      end 
+      
+      it "buyer containing name with uppercase, lowercase letters and blank spaces are valid" do
+        buyer = Buyer.new(buyer_name: "Someone", email: "someone@gmail.com", location: "somewhere", post_id: 1, user_id: 1).save
+        expect(buyer).to eq(true)
+      end
+
+      it "buyer containing name with digits or special characters is invalid" do
+        buyer = Buyer.new(buyer_name: "Someone2", email: "someone@gmail.com", location: "somewhere", post_id: 1, user_id: 1).save
+        expect(buyer).to eq(false)
+      end
+    end  
+  end  
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,47 +2,45 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   context "validations" do
+    
     describe ".name" do
-      it "category having name should be valid" do
-        category = Category.new(name: "Furniture").save
-        expect(category).to eq(true)
+      let(:category) { Category.new(name: "Furniture") }
+      let(:category_duplicate) { Category.new(name: "Books") }
+      let(:category_invalid_uniqueness) { Category.new(name: "Furniture") }
+      let(:category_invalid_name) { Category.new(name: "") }
+      let(:category_invalid_length) { Category.new(name: "Air Conditioner") }
+      let(:category_invalid_format) { Category.new(name: "Furniture2") }
+      
+      it "category having a name should be valid" do
+        expect(category).to be_valid
       end
 
       it "category having no name should be invalid" do
-        category = Category.new(name: "").save
-        expect(category).to eq(false)
+        expect(category_invalid_name).not_to be_valid
       end 
       
-      it "category having name within 2 and 14 characters should be valid" do
-        category = Category.new(name: "Furniture").save
-        expect(category).to eq(true)
+      it "category having name of length within 2 and 14 characters should be valid" do
+        expect(category).to be_valid
       end
 
-      it "category not having name within 2 and 14 characters should be invalid" do
-        category = Category.new(name: "Air Conditioner").save
-        expect(category).to eq(false)
+      it "category not having name of length within 2 and 14 characters should be invalid" do
+        expect(category_invalid_length).not_to be_valid
       end 
       
       it "categories having unique names is valid" do
-        category = Category.new(name: "Books").save
-        category = Category.new(name: "Furniture").save
-        expect(category).to eq(true)
+        expect(category_duplicate.name).not_to eq(category.name)
       end
 
-      it "categories not having unique names is valid" do
-        category = Category.new(name: "Books").save
-        category = Category.new(name: "Books").save
-        expect(category).to eq(false)
+      it "categories not having unique names is invalid" do
+        expect(category_invalid_uniqueness.name).to eq(category.name)
       end
       
       it "categories containing name with uppercase, lowercase letters and blank spaces are valid" do
-        category = Category.new(name: "Books").save
-        expect(category).to eq(true)
+        expect(category).to be_valid
       end
 
       it "categories containing name with digits or special characters is invalid" do
-        category = Category.new(name: "Books2").save
-        expect(category).to eq(false)
+        expect(category_invalid_format).not_to be_valid
       end
     end  
   end
