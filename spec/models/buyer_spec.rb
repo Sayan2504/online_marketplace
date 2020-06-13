38fd6e96a65507904cc3975de23989f325f4d478
Sayan Sinha <sinhasayan888@gmail.com>
2020-06-13 15:33:24 +0530

    updated test cases for buyer model scopes

--- spec/models/buyer_spec.rb
+++ spec/models/buyer_spec.rb
@@ -116,38 +116,35 @@ RSpec.describe Buyer, type: :model do
 
   context "scopes" do
     describe ".buyer_email" do
-      let(:buyer1) { Buyer.new(buyer_name: "Someone", email: "someone@gmail.com", location: "somewhere", post_id: 1, user_id: 1) }
-      let(:buyer2) { Buyer.new(buyer_name: "Someone", email: "someone2@gmail.com", location: "somewhere", post_id: 1, user_id: 1) }
-
       it "Buyer with the correct user email is valid" do
-        expect(buyer1.email).to include("someone@gmail.com")
+        subject.email = "someone@gmail.com"
+        expect(subject.email).to include("someone@gmail.com")
       end
       it "Buyer with the wrong user email is invalid" do
-        expect(buyer2.email).not_to include("someone@gmail.com")
+        subject.email = "someone2@gmail.com"
+        expect(subject.email).not_to include("someone@gmail.com")
       end
     end
 
     describe ".buyer_post_id" do
-      let(:buyer1) { Buyer.new(buyer_name: "Someone", email: "someone@gmail.com", location: "somewhere", post_id: 1, user_id: 1) }
-      let(:buyer2) { Buyer.new(buyer_name: "Someone", email: "someone@gmail.com", location: "somewhere", post_id: 2, user_id: 1) }
-
       it "Buyer associated with the correct post is valid" do
-        expect([buyer1.post_id]).to include(1)
+        subject.post_id = 1
+        expect([subject.post_id]).to include(1)
       end
       it "Buyer associated with the wrong post is invalid" do
-        expect([buyer2.post_id]).not_to include(1)
+        subject.post_id = 2
+        expect([subject.post_id]).not_to include(1)
       end
     end
     
     describe ".buyer_user_id" do
-      let(:buyer1) { Buyer.new(buyer_name: "Someone", email: "someone@gmail.com", location: "somewhere", post_id: 1, user_id: 1) }
-      let(:buyer2) { Buyer.new(buyer_name: "Someone", email: "someone@gmail.com", location: "somewhere", post_id: 1, user_id: 2) }
-
       it "Buyer with the correct user id is valid" do
-        expect([buyer1.user_id]).to include(1)
+        subject.user_id = 1
+        expect([subject.user_id]).to include(1)
       end
       it "Buyer with the wrong user id is invalid" do
-        expect([buyer2.user_id]).not_to include(1)
+        subject.user_id = 2
+        expect([subject.user_id]).not_to include(1)
       end
     end
   end
