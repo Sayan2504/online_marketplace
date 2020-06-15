a71d238799bbffd8d379128e222555a7c3f99edf
Sayan Sinha <sinhasayan888@gmail.com>
2020-06-15 21:29:18 +0530

    online marketplace

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,5 @@
 module ApplicationCable
-  class Connection < ActionCable::Connection::Base
+  #class Connection < ActionCable::Connection::Base
   # identified_by :current_user
   # def connect
   #    self.current_user = User.find_by(id: session["user_id"])
@@ -8,5 +8,5 @@ module ApplicationCable
   #  def session
   #    cookies.encrypted[Rails.application.config.session_options[:key]]
   #  end
-  end
+  #end
 end
