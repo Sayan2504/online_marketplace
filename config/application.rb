b2067fe268ae4964c0499cdfa44ed82fbaf43ed3
Sayan Sinha <sinhasayan888@gmail.com>
2020-06-16 20:42:00 +0530

    online marketplace

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module OnlineMarketplace
 
     config.time_zone = 'Kolkata'    
     config.assets.initialize_on_precompile = false
+    config.secret_key_base = '6ecdfe98befc5bcd6d36874f588422a4c712e185cb39c6e3283cb84373bff81fdc3bdf7e5bfa73d3b62e5bce18d517e5631a0e417c85638ea6142d124743a29b'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
