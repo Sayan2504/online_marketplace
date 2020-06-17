071122c602d8a73d4b9acad487d0e4abed8be7e3
Sayan Sinha <sinhasayan888@gmail.com>
2020-06-17 22:15:15 +0530

    online marketplace

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, '1480998798729077', 'f265cc10c79ca893f821d79ba17236ff'
-    provider :google_oauth2, '463463711280-vdfeo1ahqp02a626avn5k3qri817jpru.apps.googleusercontent.com', 'OuWIm8B9haH5nOggxCRK3A3w'
+    provider :google_oauth2, '463463711280-bdpic0fsl75u99c0a8pgoi3ij8b3tj08.apps.googleusercontent.com', 'Lddpg81sZAlD9UDcAoVYDBqD'
 end
 
