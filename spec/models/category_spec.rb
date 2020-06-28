f769e7f4111ee42d7630d95f7375743d46d2b777
Sayan Sinha <sinhasayan888@gmail.com>
2020-06-28 15:16:17 +0530

    updated rspec category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,42 +2,67 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   subject { create(:category) }
-  context "validations" do
-    describe ".name" do
-      let(:category_duplicate) { create(:categorynew) }
-      
+
+  #validation tests
+  describe ".category_name_presence" do
+    context "category name present" do
       it "category having a name is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "category name not present" do
       it "category having no name is invalid" do
         subject.name = ""
         expect(subject).not_to be_valid
       end 
+    end
+  end
+
+  describe ".category_name_length" do
+    context "valid category name length" do
       it "category having name of length within 2 and 14 characters is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "invalid category name length" do
       it "category not having name of length within 2 and 14 characters is invalid" do
         subject.name = "Air Conditioner"
         expect(subject).not_to be_valid
       end 
+    end
+  end
+
+  describe ".category_name_uniqueness" do
+    let(:category_duplicate) { create(:categorynew) }
+    context "unique category name" do
       it "category having unique name is valid" do
         expect(category_duplicate.name).not_to eq(subject.name)
       end
+    end
+    context "non-unique category name" do
       it "category not having unique name is invalid" do
         subject.name = "Someone again"
         expect(category_duplicate.name).to eq(subject.name)
       end
+    end
+  end
+
+  describe ".category_name_format" do
+    context "valid category name format" do
       it "category containing name with uppercase, lowercase letters and blank spaces is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "invalid category name format" do
       it "category containing name with digits or special characters is invalid" do
         subject.name = "Furniture2"
         expect(subject).not_to be_valid
       end
-    end  
-  end
+    end
+  end  
 
-  context "associations" do
+  #association tests
+  describe "has_many :posts" do
     it { is_expected.to have_many :posts }
-  end 
+  end
 end
