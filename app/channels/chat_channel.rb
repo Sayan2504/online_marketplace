6df2cbc2a3e0d4b802d68c567fa0acdd91af4fcc
Sayan Sinha <sinhasayan888@gmail.com>
2020-06-28 12:57:15 +0530

    updated rspec channel structure

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,6 +1,5 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
     stream_from "chat_channel_#{current_user.id}"
   end
 
--- spec/channels/chat_channel_spec.rb
+++ spec/channels/chat_channel_spec.rb
@@ -2,21 +2,32 @@ require 'rails_helper'
 
 RSpec.describe ChatChannel, type: :channel do
   let (:user1) { create(:user) }
-  
+  let (:user2) { create(:usernew) }
+
   before { stub_connection current_user: user1 }
   
   describe "#subscribed" do
-    it "successfully subscribes" do
-      subscribe()
-      expect(subscription).to have_stream_from("chat_channel_#{user1.id}")
-    end
+    context "for subscribed" do
+      it "successfully subscribes" do
+        subscribe()
+        expect(subscription).to have_stream_from("chat_channel_#{user1.id}")
+      end
+    end 
+    context "for not subscribed" do
+      it "is expected to not subscribe" do
+        subscribe()
+        expect(subscription).to_not have_stream_from("chat_channel_#{user2.id}")
+      end
+    end 
   end
   
   describe "#unsubscribed" do
-    it "is expected to successfully unsubscribed" do
-      subscribe()
-      unsubscribe
-      expect(subscription).to_not have_streams
+    context "for unsubscribe channel" do
+      it "is expected to successfully unsubscribed" do
+        subscribe()
+        unsubscribe
+        expect(subscription).to_not have_streams
+      end
     end
   end
 end
