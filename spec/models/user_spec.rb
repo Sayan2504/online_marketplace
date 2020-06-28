8dbe5a3800859238138155e71d7bf68c96b3b893
Sayan Sinha <sinhasayan888@gmail.com>
2020-06-29 00:03:26 +0530

    updated rspec user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,73 +2,128 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject { described_class.new(name: "Someone", email: "someone@gmail.com", password: "11111111") }
-  context "validations" do
-    describe ".name" do
+
+  #validation tests
+  describe ".user_name_presence" do
+    context "user name present" do
       it "user having a name is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "user name not present" do
       it "user having no name is invalid" do
         subject.name = ""
         expect(subject).not_to be_valid
       end
+    end
+  end
+
+  describe ".user_name_length" do
+    context "valid user name length" do
       it "user having name of length within 2 and 30 characters is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "invalid user name length" do
       it "user not having name of length within 2 and 30 characters is invalid" do
         subject.name = "S"
         expect(subject).not_to be_valid
       end 
+    end
+  end
+
+  describe ".user_name_format" do
+    context "valid user name format" do
       it "user containing name with uppercase, lowercase letters and blank spaces is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "invalid user name format" do
       it "user containing name with digits or special characters is invalid" do
         subject.name = "Someone1"
         expect(subject).not_to be_valid
       end
     end
-    describe ".email" do
-      let(:user_duplicate) { User.new(name: "Someone", email: "some@gmail.com", password: "11111111") }
-      let(:user_invalid_uniqueness) { User.new(name: "Someone", email: "someone@gmail.com", password: "11111111") }
-      
+  end
+
+  describe ".user_email_presence" do
+    context "user email present" do
       it "user having an email is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "user email not present" do
       it "user having no email is invalid" do
         subject.email = ""
         expect(subject).not_to be_valid
       end
+    end
+  end
+
+  describe ".user_email_length" do
+    context "valid user email length" do
       it "user having email of length within 5 and 80 characters is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "invalid user email length" do
       it "user not having email of length within 5 and 80 characters is invalid" do
         subject.email = "g.in"
         expect(subject).not_to be_valid
-      end 
+      end
+    end
+  end
+  
+  describe ".user_email_uniqueness" do
+    let(:user_duplicate) { User.new(name: "Someone", email: "some@gmail.com", password: "11111111") }
+    context "unique user email" do
       it "user having unique email is valid" do
         expect(user_duplicate.email).not_to eq(subject.email)
       end
+    end
+    context "non-unique user email" do
       it "user not having unique email is invalid" do
-        expect(user_invalid_uniqueness.email).to eq(subject.email)
+        subject.email = "some@gmail.com"
+        expect(user_duplicate.email).to eq(subject.email)
       end
+    end
+  end
+
+  describe ".user_email_format" do
+    context "valid user email format" do
       it "user containing email with the format ______@_____._____is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "invalid user email format" do
       it "user not containing email with the format ______@_____._____ is invalid" do
         subject.email = "someone.com"
         expect(subject).not_to be_valid
       end
     end
-    describe "#password" do
+  end
+
+  describe "#user_password_presence" do
+    context "user password present" do
       it "user having a password is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "user password not present" do
       it "user having no password is invalid" do
         subject.password = nil
         expect(subject).not_to be_valid
       end
+    end
+  end
+
+  describe "#user_password_length" do
+    context "valid user password length" do
       it "user having password of length within 6 and 30 characters is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "invalid user password length" do
       it "user not having password of length within 6 and 30 characters is invalid" do
         subject.password = "1111"
         expect(subject).not_to be_valid
@@ -76,15 +131,28 @@ RSpec.describe User, type: :model do
     end
   end
 
-  context "associations" do
-    it { is_expected.to have_many :posts }   
-    it { is_expected.to have_many :messages }   
+  #association tests
+  describe "has_many :posts" do
+    it { is_expected.to have_many :posts } 
+  end
+  
+  describe "has_many :messages" do
+    it { is_expected.to have_many :messages }  
+  end
+  
+  describe "has_one :buyer" do
     it { is_expected.to have_one :buyer }
+  end
+
+  describe "has_many :post_attachments, through: :posts" do
     it { is_expected.to have_many :post_attachments }
+  end
+
+  describe "has_many :reviews, through: :posts" do
     it { is_expected.to have_many :reviews }
   end  
 
-  context "has_secure_password" do
+  describe "has_secure_password" do
     it { is_expected.to have_secure_password }
   end
 end
