e91f4328269cc2a547ff449dfc9bad89a9f3fdd7
Sayan Sinha <sinhasayan888@gmail.com>
2020-06-28 20:23:58 +0530

    updated rspec post_attachment model

--- spec/models/post_attachment_spec.rb
+++ spec/models/post_attachment_spec.rb
@@ -5,28 +5,37 @@ RSpec.describe PostAttachment, type: :model do
   let(:post) { create(:post1) }
   subject { described_class.new(post_id: post.id, user_id: user.id) }
 
-  context "validations" do
-    describe "#post_id" do
+  #validation tests
+  describe "#post_id" do
+    context "post id present" do
       it "post attachment associated with a post is valid" do
         expect(subject).to be_valid
       end
+    end
+    context "post id not present" do
       it "post attachment not associated with a post is invalid" do
         subject.post_id = nil
         expect(subject).not_to be_valid
       end
     end
-    describe "#user_id" do
-      it "post attachment having a sender is valid" do
+  end
+
+  describe "#user_id" do
+    context "user id present" do
+      it "post attachment associated with an user is valid" do
         expect(subject).to be_valid
       end
-      it "message not having a sender is invalid" do
+    end
+    context "user id not present" do
+      it "post attachment not associated with an user is invalid" do
         subject.user_id = nil
         expect(subject).not_to be_valid
       end
     end
   end  
 
-  context "associations" do
+  #association tests
+  describe "belongs_to :post" do
     it { is_expected.to belong_to :post }
   end  
 end
