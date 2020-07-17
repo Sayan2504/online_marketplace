217a348b00cebf66a831527f03b0d1c886c86bb3
Sayan Sinha <sinhasayan888@gmail.com>
2020-07-17 20:38:17 +0530

    created common homepage partial for admin and non admin users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 User.new( { name: "admin", email: "admin@gmail.com", password: "adminpassword", password_confirmation: "adminpassword", admin: true } ).save
+User.new( { name: "admin two", email: "admin2@gmail.com", password: "adminpassword", password_confirmation: "adminpassword", admin: true } ).save
 User.new( { name: "user one", email: "user1@gmail.com", password: "12345678", password_confirmation: "12345678" } ).save
 User.new( { name: "user two", email: "user2@gmail.com", password: "12345678", password_confirmation: "12345678" } ).save
 
